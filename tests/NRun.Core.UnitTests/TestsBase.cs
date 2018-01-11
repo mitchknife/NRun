a9effc729f94cf5f19ec273855fdcc5b88c952df
Mitch Rosenburg <mrosenburg@logos.com>
2018-01-11 10:45:29 -0800

    Wait on semaphore longer when counts are not equal.

--- tests/NRun.Core.UnitTests/TestsBase.cs
+++ tests/NRun.Core.UnitTests/TestsBase.cs
@@ -47,7 +47,7 @@ namespace NRun.Core.UnitTests
 		public static void ShouldWait(this SemaphoreSlim semaphore, int count)
 		{
 			int actualCount = 0;
-			while (semaphore.Wait(100))
+			while (semaphore.Wait(actualCount == count ? 100 : 1000))
 				actualCount++;
 
 			actualCount.Should().Be(count, "semaphore.Release() should have been called {0} times", count);
