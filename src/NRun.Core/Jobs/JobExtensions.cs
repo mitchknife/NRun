dd27bca08a21dcf95a536155982c308b7ee2cd89
Mitch Rosenburg <mrosenburg@logos.com>
2018-01-12 06:50:42 -0800

    Remove SequentialJob and ParallelJob.
    - SequentialJob is too simple and easy to do on your own.
    - ParallelJob is too complicated due to threading and exception handling, so maybe we add it back in later when we have a better use case.

--- src/NRun.Core/Jobs/JobExtensions.cs
+++ src/NRun.Core/Jobs/JobExtensions.cs
@@ -1,28 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace NRun.Core.Jobs
 {
 	public static class JobExtensions
 	{
-		/// <summary>
-		/// Converts a sequence of jobs into a parallel job.
-		/// </summary>
-		/// <param name="jobs">The jobs to execute in parallel.</param>
-		/// <returns></returns>
-		public static ParallelJob ToParallelJob(this IEnumerable<IJob> jobs)
-		{
-			return new ParallelJob(jobs);
-		}
-		/// <summary>
-		/// Converts a sequence of jobs to a sequential job.
-		/// </summary>
-		/// <param name="jobs">The jobs to execute sequentially.</param>
-		public static SequentialJob ToSequentialJob(this IEnumerable<IJob> jobs)
-		{
-			return new SequentialJob(jobs);
-		}
-
 		/// <summary>
 		/// Converts an observable sequence of jobs to a streaming job.
 		/// </summary>
