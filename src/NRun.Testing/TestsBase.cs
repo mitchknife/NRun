1ed7234c147e32a09b8f89b7cdc95a676ec10f18
Mitch Rosenburg <mrosenburg@faithlife.com>
2022-09-14 13:07:27 -0700

    Fix tabs.

--- src/NRun.Testing/TestsBase.cs
+++ src/NRun.Testing/TestsBase.cs
@@ -5,39 +5,39 @@ using FluentAssertions;
 
 namespace NRun.Testing
 {
-    public abstract class TestsBase
-    {
-        protected Action Invoking(Action action)
-        {
-            return this.Invoking(_ => action());
-        }
+	public abstract class TestsBase
+	{
+		protected Action Invoking(Action action)
+		{
+			return this.Invoking(_ => action());
+		}
 
-        protected Func<Task> Awaiting(Func<Task> funcAsync)
-        {
-            return this.Awaiting(_ => funcAsync());
-        }
+		protected Func<Task> Awaiting(Func<Task> funcAsync)
+		{
+			return this.Awaiting(_ => funcAsync());
+		}
 
-        protected Func<CancellationToken, Task> CreateExecuteAsync(Action<CancellationToken> execute)
-        {
-            return async ct =>
-            {
-                await Task.Delay(1).ConfigureAwait(false);
-                execute(ct);
-            };
-        }
+		protected Func<CancellationToken, Task> CreateExecuteAsync(Action<CancellationToken> execute)
+		{
+			return async ct =>
+			{
+				await Task.Delay(1).ConfigureAwait(false);
+				execute(ct);
+			};
+		}
 
-        protected Func<CancellationToken, Task> CreateExecuteAsync(Action start, Action stop)
-        {
-            return async ct =>
-            {
-                if (!ct.CanBeCanceled)
-                    throw new InvalidOperationException("cancellationToken must be able to be cancelled.");
-                start();
-                var taskCompletion = new TaskCompletionSource<bool>();
-                using (ct.Register(() => taskCompletion.SetResult(true)))
-                    await taskCompletion.Task.ConfigureAwait(false);
-                stop();
-            };
-        }
-    }
+		protected Func<CancellationToken, Task> CreateExecuteAsync(Action start, Action stop)
+		{
+			return async ct =>
+			{
+				if (!ct.CanBeCanceled)
+					throw new InvalidOperationException("cancellationToken must be able to be cancelled.");
+				start();
+				var taskCompletion = new TaskCompletionSource<bool>();
+				using (ct.Register(() => taskCompletion.SetResult(true)))
+					await taskCompletion.Task.ConfigureAwait(false);
+				stop();
+			};
+		}
+	}
 }
